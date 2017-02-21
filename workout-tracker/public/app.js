16790ee55850293f12de9b3c7da8d162234145ab
SirBLT <blt8324@gmail.com>
2017-02-20 19:56:05 -0700

    ui router working

--- workout-tracker/public/app.js
+++ workout-tracker/public/app.js
@@ -1,1 +1,27 @@
-angular.module('workout', [])
+angular.module('workout', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    controller: 'mainCtrl',
+    templateUrl: 'views/home.html'
+  })
+  .state('login', {
+    url: '/login',
+    controller: 'mainCtrl',
+    templateUrl: 'views/login.html'
+  })
+  .state('workout', {
+    url: '/workout',
+    controller: 'mainCtrl',
+    templateUrl: 'views/workout-index.html'
+  })
+  .state('logWorkout', {
+    url: '/log-workout',
+    controller: 'mainCtrl',
+    templateUrl: '/views/log-workout.html'
+  })
+})
